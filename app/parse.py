a9401318875621e9917ce97d417c581fb7799e10
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-11-12 16:14:52 +0200

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,21 +1,10 @@
-import csv
 from dataclasses import dataclass
-
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 BASE_URL = "https://quotes.toscrape.com/"
-
-
-def safe_request(url: str) -> requests.Response:
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response
-    except requests.RequestException as e:
-        print(f"Error fetching {url}: {e}")
-        return None
 
 
 @dataclass
@@ -23,14 +12,6 @@ class Quote:
     text: str
     author: str
     tags: list[str]
-
-
-@dataclass
-class Author:
-    name: str
-    biography: str
-    birth_date: str
-    birth_place: str
 
 
 def parse_single_page(page_content: str) -> list[Quote]:
@@ -47,50 +28,45 @@ def parse_single_page(page_content: str) -> list[Quote]:
 
 def get_num_pages() -> int:
     page_number = 1
-
     while True:
-        response = safe_request(f"{BASE_URL}page/{page_number}")
-        if not response:
-            break
-
-        soup = BeautifulSoup(response.content, "html.parser")
+        page = requests.get(f"{BASE_URL}page/{page_number}/")
+        soup = BeautifulSoup(page.content, "html.parser")
         if not soup.select_one(".next"):
             break
         page_number += 1
     return page_number
 
 
-def write_to_csv(data: list, output_csv_path: str, fieldnames: list) -> None:
-    with open(output_csv_path, "w", encoding="utf-8", newline="") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for item in data:
-            writer.writerow(item)
-
-
 def get_all_quotes() -> list[Quote]:
     quotes = []
     num_pages = get_num_pages()
     for i in range(1, num_pages + 1):
-        response = safe_request(f"{BASE_URL}page/{i}")
-        if not response:
-            continue
-        quotes.extend(parse_single_page(response.content))
+        page = requests.get(f"{BASE_URL}page/{i}/")
+        quotes.extend(parse_single_page(page.content))
     return quotes
 
 
 def main(output_csv_path: str) -> None:
     quotes = get_all_quotes()
 
-    quotes_data = [
-        {
-            "text": quote.text,
-            "author": quote.author,
-            "tags": ", ".join(quote.tags)
-        }
-        for quote in quotes
-    ]
-    write_to_csv(quotes_data, output_csv_path, ["text", "author", "tags"])
+    with open(
+            output_csv_path,
+            mode="w",
+            encoding="utf-8",
+            newline=""
+    ) as csv_file:
+        writer = csv.DictWriter(
+            csv_file,
+            fieldnames=["text", "author", "tags"]
+        )
+        writer.writeheader()
+
+        for quote in quotes:
+            writer.writerow({
+                "text": quote.text,
+                "author": quote.author,
+                "tags": quote.tags,
+            })
 
 
 if __name__ == "__main__":
