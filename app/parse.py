1866f9dc9bd3fb79f69dd7a686bb5d67b21d2127
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-11-12 15:59:57 +0200

    solution

--- app/parse.py
+++ app/parse.py
@@ -23,7 +23,6 @@ class Quote:
     text: str
     author: str
     tags: list[str]
-    author_url: str
 
 
 @dataclass
@@ -38,14 +37,10 @@ def parse_single_page(page_content: str) -> list[Quote]:
     soup = BeautifulSoup(page_content, "html.parser")
     quotes = []
     for quote in soup.select(".quote"):
-        author_url = (
-            BASE_URL + quote.select_one("a[href^='/author/']").get("href")
-        )
         quotes.append(Quote(
             text=quote.select_one(".text").text,
             author=quote.select_one(".author").text,
             tags=[tag.text for tag in quote.select(".tag")],
-            author_url=author_url,
         ))
     return quotes
 
@@ -63,19 +58,6 @@ def get_num_pages() -> int:
             break
         page_number += 1
     return page_number
-
-
-def get_author_biography(author_url: str) -> Author:
-    response = safe_request(author_url)
-    if not response:
-        return None
-
-    soup = BeautifulSoup(response.content, "html.parser")
-    name = soup.select_one(".author-title").text.strip()
-    biography = soup.select_one(".author-description").text.strip()
-    birth_date = soup.select_one(".author-born-date").text.strip()
-    birth_place = soup.select_one(".author-born-location").text.strip()
-    return Author(name, biography, birth_date, birth_place)
 
 
 def write_to_csv(data: list, output_csv_path: str, fieldnames: list) -> None:
@@ -109,28 +91,6 @@ def main(output_csv_path: str) -> None:
         for quote in quotes
     ]
     write_to_csv(quotes_data, output_csv_path, ["text", "author", "tags"])
-
-    authors = {}
-    for quote in quotes:
-        if quote.author not in authors:
-            author_data = get_author_biography(quote.author_url)
-            if author_data:
-                authors[quote.author] = author_data
-
-    authors_data = [
-        {
-            "name": author.name,
-            "biography": author.biography,
-            "birth_date": author.birth_date,
-            "birth_place": author.birth_place
-        }
-        for author in authors.values()
-    ]
-    write_to_csv(
-        authors_data,
-        "authors.csv",
-        ["name", "biography", "birth_date", "birth_place"]
-    )
 
 
 if __name__ == "__main__":
